refactor, still a bug in value computation to work out

draft.py
@@ -3,6 +3,9 @@ import loaddata
 import params as p
 import value
 
+#Load data and compute the valuation function
+lrv = value.LinRegValuation()
+
 #####################
 # Price 2015 players for 2016 draft
 # (B, P) = NL2016Draft()
@@ -19,13 +22,15 @@ def nl_batter_vorp2016():
     # where rows of B are first name,last name,pos,vorp,id,s,t,a,t,s
 
     ##Load player performance data and compute values
-    batval = value.batter_value_fun()
-    B = loaddata.NLBatters2015()
-    names = loaddata.NLNames2015()
+    db2015 = loaddata.LahmanNL2015()
+    db2015.connect()
+    B = db2015.NLBatters2015()
+    names = db2015.NLNames2015()
+    db2015.disconnect()
 
     nBatters=int(np.ceil(p.teamSize/2.0)) #expected number of batters on a team
 
-    Bval = [batval(x) for x in B]
+    Bval = [lrv.bvalfcn(x) for x in B]
     Bval = value.abs_to_vorp(Bval, p.numTeams * nBatters)
     Bval = value.to_samolians(Bval, nBatters)
 
@@ -42,13 +47,15 @@ def nl_pitcher_vorp2016():
     # where rows of P are first name,last name,pos,vorp,id,s,t,a,t,s
 
     ##Load player performance data and compute values
-    pitval = value.pitcher_value_fun()
-    P = loaddata.NLPitchers2015()
-    names = loaddata.NLNames2015()
-
+    db2015=loaddata.LahmanNL2015()
+    db2015.connect()
+    P = db2015.NLPitchers2015()
+    names = db2015.NLNames2015()
+    db2015.disconnect()
+    
     nPitchers=int(np.floor(p.teamSize/2.0)) # expected number of pitcher on a team
 
-    Pval = [pitval(x) for x in P]
+    Pval = [lrv.pvalfcn(x) for x in P]
     Pval = value.abs_to_vorp(Pval, p.numTeams * nPitchers)
     Pval = value.to_samolians(Pval, nPitchers) 
 

value.py
@@ -1,32 +1,71 @@
-import params as p
-import numpy as np
+import params
+import numpy
 import loaddata
 
+########################################
+## Class for building valuation tools ##
+########################################
+
+class Valuation(object):
+    
+    def __init__(self):
+        return
+    
+    def value_batters(self, batters):
+        # Compute the value of a list of batters
+        return [0.0]*len(players)
+
+    def value_pitchers(self, pitchers):
+        # Compute the value of a list of batters
+        return [0.0]*len(players)
+
+#####################################
+## Valuations by linear regression ##
+#####################################
+    
+class LinRegValuation(Valuation):
+    # Value players by linear regression
+    def __init__(self):
+        db = loaddata.LahmanDB()
+        db.connect()
+        
+        self.bvalfcn = self._linreg(db.batters(),rcond=0.05) #f:(batter s,t,a,t,s)->values
+        self.pvalfcn = self._linreg(db.pitchers(),rcond=0.05) #f:(pitching s,t,a,t,s)->values
+
+        db.disconnect
+        
+        return
+        
+    def _linreg(self,data,rcond=-1):
+        # Fit a linear function to the batters or pitchers data,
+        # drop singular values smaller than rcond*(spectral norm)
+        #    f = linreg(data)
+        #    f = linreg(data,rcond=0.01)
+        
+        xy = list(zip(*[(x[1][1:], x[1][0]) for x in data]))
+        coeff = numpy.linalg.lstsq(xy[0], xy[1],rcond)
+        def pricehim(x):
+            return sum([coeff[0][i] * x[1][i+1] for i in range(0,len(coeff))])
+        return pricehim
+        
 #####################
 ## Tools for valuing players
 
-def stats_to_vals(stats):
-    #determine a single value from a list of stats for each player
+def normalize_stats(stats):
+    #determine a single value from a list of stats for all players
     # vals = stats_to_vals(stats)
     # each row of stats is the collection of statistics (larger better) for the players
     # the stats are normalized to the params.normPct percentile and summed for each player to get value
-    norm = np.percentile(stats,p.normPct,axis=0)
+    norm = numpy.percentile(stats,params.normPct,axis=0)
     vals = [sum([1.0*x[i] / norm[i] for i in range(0,6)]) for x in stats]
     return vals
 
-def abs_to_vorp(absval, k):
+def abs_to_vorp(absval, repval):
     #convert "absolute" player values to "value over replacement player"
     #vorp = abs_to_vorp(absval,k)
-    #replacement player is taken to be the (k+1)th by value
+    #repval - value of hypothetical replacement player
 
-    abscopy = list(absval)
-    abscopy.sort() #sorted least to highest valuable
-    if (k+1) > len(absval):
-        rpval = 0
-    else:
-        rpval = abscopy[-(k+1)]
-
-    vorp = [x-rpval for x in absval]
+    vorp = [x-repval for x in absval]
     return vorp
 
 def to_samolians(vals, nPlayers):
@@ -36,33 +75,9 @@ def to_samolians(vals, nPlayers):
     # nPlayers - number of these players on each team
     # sams - values in samolians
 
-    cf = 1.0 * p.teamBudget * p.numTeams *nPlayers / (p.teamSize * sum(np.sort(vals)[-(nPlayers*p.numTeams):]))
+    cf = 1.0 * params.teamBudget * params.numTeams #Total amount of samolians
+    cf *= nPlayers / (params.teamSize) #times fraction of total for these players
+    cf /= sum(numpy.sort(vals)[-(nPlayers*params.numTeams):]) #divided by sum of values
+
     return [x*cf for x in vals]
 
-def batter_value_fun():
-    #fit a linear valuation function to the batters data
-    #f = batter_value_fun()
-    #f:(batter s,t,a,t,s)->values
-    
-    return value___linreg(loaddata.batters(),rcond=0.05)
-
-def pitcher_value_fun():
-    #fit a linear value function to the pitching data
-    #f = pitching_value_fun()
-    #f:(pitching s,t,a,t,s)->values
-    
-    return value___linreg(loaddata.pitchers(),rcond=0.05)
-
-########################
-### Helper functions ###
-########################
-def value___linreg(data,rcond=-1):
-    #Fit a linear function to the batters or pitchers data,
-    #drop singular values smaller than rcond*(spectral norm)
-    #f = draftvalues___linreg(data)
-    #f = draftvalues___linreg(data,rcond=0.01)
-    xy = list(zip(*[(x[1][1:], x[1][0]) for x in data]))
-    coeff = np.linalg.lstsq(xy[0], xy[1],rcond)
-    def pricehim(x):
-        return sum([coeff[0][i] * x[1][i+1] for i in range(0,len(coeff))])
-    return pricehim